ae6d84709fed79182adb6698e4730c633070a916
M22AIE222 <m22aie222@iitj.ac.in>
2024-04-04 23:16:47 +0530

    modifying exsisting files

--- ActivationFucntion.py
+++ ActivationFucntion.py
@@ -1,9 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-# Define the activation functions
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
 
 def relu(x):
     return np.maximum(0, x)
@@ -14,42 +9,12 @@ def leaky_relu(x, alpha=0.01):
 def tanh(x):
     return np.tanh(x)
 
-# Generate a range of values for plotting
-x = np.linspace(-5, 5, 100)
+random_values = [-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6]
 
-# Apply each activation function to these values
-y_sigmoid = sigmoid(x)
-y_relu = relu(x)
-y_leaky_relu = leaky_relu(x)
-y_tanh = tanh(x)
+relu_values = [relu(x) for x in random_values]
+leaky_relu_values = [leaky_relu(x) for x in random_values]
+tanh_values = [tanh(x) for x in random_values]
 
-# Create a plot for each activation function
-plt.figure(figsize=(12, 8))
-
-# Sigmoid
-plt.subplot(2, 2, 1)
-plt.plot(x, y_sigmoid, label="Sigmoid", color='blue')
-plt.title("Sigmoid Function")
-plt.grid()
-
-# ReLU
-plt.subplot(2, 2, 2)
-plt.plot(x, y_relu, label="ReLU", color='orange')
-plt.title("ReLU Function")
-plt.grid()
-
-# Leaky ReLU
-plt.subplot(2, 2, 3)
-plt.plot(x, y_leaky_relu, label="Leaky ReLU", color='green')
-plt.title("Leaky ReLU Function")
-plt.grid()
-
-# Tanh
-plt.subplot(2, 2, 4)
-plt.plot(x, y_tanh, label="Tanh", color='red')
-plt.title("Tanh Function")
-plt.grid()
-
-plt.tight_layout()
-plt.show()
-
+print("ReLU, Leaky ReLU, and Tanh values for the given data points:")
+for x, relu_val, leaky_relu_val, tanh_val in zip(random_values, relu_values, leaky_relu_values, tanh_values):
+    print(f"ReLU({x}) = {relu_val}, Leaky ReLU({x}) = {leaky_relu_val}, Tanh({x}) = {tanh_val}")
